f908c1897af98131fa133e59bf6de1210f145cbd
DominykMcNamara <dominyksmith@gmail.com>
2024-03-19 23:51:03 -0400

    Basic NavBar.vue

--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -3,15 +3,39 @@
 
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import Dropdown from "@/Components/Dropdown.vue";
 </script>
 
 <template>
-    <nav class="flex justify-around w-96">
-        <div class="my-auto">
-            <ApplicationLogo id="application-logo"/>
-        </div>
-        <div class="my-auto">
-            <ResponsiveNavLink href="/"> Home</ResponsiveNavLink>
-        </div>
+    <nav class="flex justify-around w-[100vw] mx-auto py-2 border-b  border-gray-100 drop-shadow-2xl sticky">
+
+        <ApplicationLogo id="application-logo"/>
+
+        <ul class="w-96 flex  justify-around my-auto">
+            <li>
+                <ResponsiveNavLink :active="$page.component === 'Home'" href="/">Home</ResponsiveNavLink>
+            </li>
+            <li v-if="!$page.props.auth.user">
+                <ResponsiveNavLink :active="$page.component === 'Login'" href="/login">Log In</ResponsiveNavLink>
+            </li>
+            <li v-if="!$page.props.auth.user">
+                <ResponsiveNavLink :active="$page.component === 'Register'" href="/register">Register
+                </ResponsiveNavLink>
+            </li>
+            <li v-if="$page.props.auth.user">
+                <ResponsiveNavLink :active="$page.component === 'Home'" href="/dashboard"> Welcome Back   {{
+                        $page.props.auth.user.name }}
+
+                </ResponsiveNavLink>
+            </li>
+
+
+            <li v-if="$page.props.auth.user">
+                <ResponsiveNavLink href="/logout"> Logout</ResponsiveNavLink>
+            </li>
+
+        </ul>
+
     </nav>
 </template>
